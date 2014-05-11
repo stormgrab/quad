8bfb19b890d25d071b9c4e9b16b9a3204a7daa39
Lakshya Ranganath <lakshya93@gmail.com>
2014-05-11 11:03:54 +0530

    Added navbar

---
+++ includes/navbar.php
@@ -0,0 +1,56 @@
+<div class="container">
+
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">QUAD</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-left">
+					<li><a href="#">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Consultancy</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Supply <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li class="dropdown-submenu"> <a tabindex="-1" href="#">Raw Materials </a>
+								<ul class="dropdown-menu">
+									<li><a tabindex="-1" href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+								</ul>
+							</li>
+							<li class="divider"></li>
+							<li class="dropdown-submenu"> <a tabindex="-1" href="#">Consumables </a>
+								<ul class="dropdown-menu">
+									<li><a tabindex="-1" href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+								</ul>
+							</li>
+							<li class="divider"></li>
+							<li class="dropdown-submenu"> <a tabindex="-1" href="#">Machinery & Spares </a>
+								<ul class="dropdown-menu">
+									<li><a tabindex="-1" href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+									<li><a href="#">Level 2</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="#">Exclusive Products</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+		<head>
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+				<title></title>
+				<meta name="description" content="">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+
+				<link rel="stylesheet" href="css/bootstrap.min.css">
+				<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+				<link rel="stylesheet" href="css/main.css">
+
+				<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		</head>
+		<body>
+				<!--[if lt IE 7]>
+						<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+				<![endif]-->
+
+		<!-- Navbar -->
+		<div class="navbar-wrapper">
+			<?php include 'includes/navbar.php'; ?>
+		</div>
+
+
+		<!-- Carousel -->
+		<?php include 'includes/mainslider.php'; ?>
+
+
+
+		<!-- Marketing messaging and featurettes
+		================================================== -->
+		<!-- Wrap the rest of the page in another container to center all the content. -->
+
+		<div class="container marketing">
+
+			<!-- Three columns of text below the carousel -->
+			<div class="row">
+				<div class="col-lg-4">
+					<img class="img-circle" data-src="holder.js/140x140" alt="Generic placeholder image">
+					<h2>Heading</h2>
+					<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div><!-- /.col-lg-4 -->
+				<div class="col-lg-4">
+					<img class="img-circle" data-src="holder.js/140x140" alt="Generic placeholder image">
+					<h2>Heading</h2>
+					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div><!-- /.col-lg-4 -->
+				<div class="col-lg-4">
+					<img class="img-circle" data-src="holder.js/140x140" alt="Generic placeholder image">
+					<h2>Heading</h2>
+					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div><!-- /.col-lg-4 -->
+			</div><!-- /.row -->
+
+
+			<!-- START THE FEATURETTES -->
+
+			<hr class="featurette-divider">
+
+			<div class="row featurette">
+				<div class="col-md-7">
+					<h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+				</div>
+				<div class="col-md-5">
+					<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+				</div>
+			</div>
+
+			<hr class="featurette-divider">
+
+			<div class="row featurette">
+				<div class="col-md-5">
+					<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+				</div>
+				<div class="col-md-7">
+					<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+				</div>
+			</div>
+
+			<hr class="featurette-divider">
+
+			<div class="row featurette">
+				<div class="col-md-7">
+					<h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+				</div>
+				<div class="col-md-5">
+					<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+				</div>
+			</div>
+
+			<hr class="featurette-divider">
+
+			<!-- /END THE FEATURETTES -->
+
+
+			<!-- FOOTER -->
+			<footer>
+				<p class="pull-right"><a href="#">Back to top</a></p>
+				<p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+			</footer>
+
+		</div> <!-- /container -->        
+
+
+		<!-- Scripts -->
+				<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+				<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+				<script src="js/vendor/bootstrap.min.js"></script>
+
+				<script src="js/main.js"></script>
+
+				<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+				<script>
+						(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+						function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+						e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+						e.src='//www.google-analytics.com/analytics.js';
+						r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+						ga('create','UA-XXXXX-X');ga('send','pageview');
+				</script>
+		</body>
+</html>
